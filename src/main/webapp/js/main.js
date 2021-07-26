241b553bde9eb7c186e40d6a2d918f55885ac6b2
Selim <selimemretoy@gmail.com>
2021-07-26 20:52:14 +0300

    Integrate new conference room design

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,62 @@
+//Video Modal *******************************************************************
+$(document).ready(function () {
+
+  // Gets the video src from the data-src on each button
+
+  var $videoSrc;
+  $('.video-btn').click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
+
+
+
+  // when the modal is opened autoplay it  
+  $('#myModal').on('shown.bs.modal', function (e) {
+
+    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+  })
+
+
+
+  // stop playing the youtube video when I close the modal
+  $('#myModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video").attr('src', $videoSrc);
+  })
+
+  // document ready  
+});
+
+// camera and microphone on off
+$(document).ready(function() {
+  $( ".camera-on" ).click(function() {
+    $( this ).removeClass( "active" );
+    $(this).next().addClass( "active" );
+  });
+  $( ".camera-off" ).click(function() {
+    $( this ).removeClass( "active" );
+    $(this).prev().addClass( "active" );
+  });
+  $( ".microphone-on" ).click(function() {
+    $( this ).removeClass( "active" );
+    $(this).next().addClass( "active" );
+  });
+  $( ".microphone-off" ).click(function() {
+    $( this ).removeClass( "active" );
+    $(this).prev().addClass( "active" );
+  });
+});
+
+// chat on off
+$(document).ready(function() {
+  $( ".chat-on" ).click(function() {
+    $( ".chat-active"  ).toggleClass( "active" );
+    $( ".chat"  ).toggleClass( "active" );
+  });
+  $( ".chat-close" ).click(function() {
+    $( ".chat-active"  ).removeClass( "active" );
+    $( ".chat"  ).removeClass( "active" );
+  });
+});
