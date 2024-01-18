19480ddedad50e21deeec1c949b72e5946349222
mekya <ahmetmermerkaya@gmail.com>
2024-01-18 18:30:45 +0300

    Enable test code for muteSpeaking

--- src/test/js/test/webrtc_adaptor.test.js
+++ src/test/js/test/webrtc_adaptor.test.js
@@ -719,9 +719,7 @@ describe("WebRTCAdaptor", function() {
 			});
 		  });
 		  
-		  return speakingButMuted;
-		  /*
-		  
+		 		  
 		  let soundMeteraddModuleFailed = speakingButMuted.then(() => {
 			adaptor.mediaManager.mutedSoundMeter.context.audioWorklet.addModule = async () => {
 				return Promise.reject("error");
@@ -734,7 +732,7 @@ describe("WebRTCAdaptor", function() {
 
 
 		  return soundMeteraddModuleFailed;
-			*/
+			
 
     });
 
