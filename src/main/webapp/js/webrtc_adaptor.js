de4d0ab3cfd2f77a257c26a951c6fb4785a10c25
Mustafa BOLEKEN <mustafa.boleken@antmedia.io>
2024-01-22 18:18:08 +0300

    Change parameter type of openStream

--- src/main/webapp/js/webrtc_adaptor.js
+++ src/main/webapp/js/webrtc_adaptor.js
@@ -2049,7 +2049,7 @@ export class WebRTCAdaptor {
     }
     /**
      *
-     * @param {object} mediaConstraints : media constraints to be used for opening the stream
+     * @param {MediaTrackConstraints} mediaConstraints : media constraints to be used for opening the stream
      * @param {string} streamId : id of the stream to replace tracks with
      * @returns
      */
