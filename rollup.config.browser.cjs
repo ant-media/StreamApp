018226da3e3ac6eaa83f41e10987f3e1bebd8832
golgetahir <tahir.golge@gmail.com>
2025-12-29 10:23:49 +0300

    Fix rollup config for mediapipe

--- rollup.config.browser.cjs
+++ rollup.config.browser.cjs
@@ -1,16 +1,17 @@
-
 const babel = require('@rollup/plugin-babel').default;
 
 const builds = {
-	input: [ 'src/main/js/index.js'],
-	output: [{
-		name: 'webrtc_adaptor',
-		file: 'dist/browser/webrtc_adaptor.js',
-		format: 'umd'
-	},
-	],
-	plugins: [babel({ babelHelpers: 'bundled' })]
-
+  input: ['src/main/js/index.js'],
+  output: [{
+    name: 'webrtc_adaptor',
+    file: 'dist/browser/webrtc_adaptor.js',
+    format: 'umd',
+    globals: {
+      '@mediapipe/selfie_segmentation': 'SelfieSegmentation'
+    }
+  }],
+  external: ['@mediapipe/selfie_segmentation'],
+  plugins: [babel({ babelHelpers: 'bundled' })]
 };
 
-module.exports = builds
+module.exports = builds;
